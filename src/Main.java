feedeb0d0f9016d56352ef6990b17254ebe090a6
ruslankorotkov <ruslankorotkov.rus@yandex.ru>
2022-12-25 12:06:01 +0300

    программа, которая проверяет, является ли строка палиндромом. применение классов.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Service palindrome = new Service(" Лег на храм, и дивен и невидим архангел. ");
+        palindrome.printSearchPalindrome(palindrome.words);
     }
 }
