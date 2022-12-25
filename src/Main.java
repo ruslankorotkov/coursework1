5a040abcfa63dd424ec10403cbd19d5638fd3d05
ruslankorotkov <ruslankorotkov.rus@yandex.ru>
2022-12-25 12:09:02 +0300

    программа, которая проверяет, является ли строка палиндромом. применение метода.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
+
 public class Main {
+    public static void searchPalindrome(String words) {
+        String words1 = words;
+        words1 = words.replaceAll("[^A-Za-zА-Яа-я0-9]", "");
+        String string = words1;
+        String words2 = new StringBuffer(string).reverse().toString();
+        if (words1.equalsIgnoreCase(words2)) {
+            System.out.println(words + " = УСПЕХ - это палиндром. ");
+        } else {
+            System.out.println(words + " = НЕУДАЧА - это не палиндром. ");
+        }
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String palindrome = " Лег на храм, и дивен и невидим архангел ";
+        searchPalindrome(palindrome);
     }
 }
